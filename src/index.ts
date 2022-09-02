9c313c99d465b7fd33e4cda8b144fa368ca7e3fe
Pavol Noha <pavol.noha@gmail.com>
2022-09-02 19:45:29 +0200

    Export types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { Router } from './api';
 export { PolkadotPoolService, PoolFactory, XykPool } from './pool';
+export * from './types';
 export * from './utils/bignumber';
-export * from './utils/math';
