1717f870261844f3e03da9418aaa72fdd34f2d66
Pavol Noha <pavol.noha@gmail.com>
2022-09-02 12:11:46 +0200

    Configure esbuild alias paths + ts config declaration for ts-types

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,46 +1,37 @@
-import esbuild from "esbuild";
-import { existsSync, mkdirSync, readdirSync, statSync, writeFileSync } from "fs";
-import { join } from "path";
+import esbuild from 'esbuild';
+import { esbuildPluginAliasPath } from 'esbuild-plugin-alias-path';
 
-const dist = join(process.cwd(), "dist");
-
-if (!existsSync(dist)) {
-  mkdirSync(dist);
-}
-
-const entryPoints = readdirSync(join(process.cwd(), "src"))
-  .filter((file) => file.endsWith(".ts") && statSync(join(process.cwd(), "src", file)).isFile())
-  .map((file) => `src/${file}`);
-
-// ESM bundles with code splitting
+// ESM bundle
 esbuild
   .build({
-    entryPoints,
-    outdir: "dist/esm",
+    entryPoints: ['src/index.ts'],
+    outdir: 'dist/esm',
     bundle: true,
     sourcemap: true,
     minify: true,
     splitting: true,
-    format: "esm",
-    target: ["esnext"],
+    plugins: [
+      esbuildPluginAliasPath({
+        alias: {
+          'hydra-dx-wasm/build/xyk/nodejs':
+            'node_modules/hydra-dx-wasm/build/xyk/bundler',
+        },
+      }),
+    ],
+    format: 'esm',
+    target: ['esnext'],
   })
   .catch(() => process.exit(1));
 
 // CJS bundle
 esbuild
   .build({
-    entryPoints: ["src/index.ts"],
-    outfile: "dist/cjs/index.cjs.js",
+    entryPoints: ['src/index.ts'],
+    outfile: 'dist/cjs/index.js',
     bundle: true,
     sourcemap: true,
     minify: true,
-    platform: "node",
-    target: ["node18"],
+    platform: 'node',
+    target: ['node18'],
   })
   .catch(() => process.exit(1));
-
-// An entry file for CJS
-writeFileSync(join(dist, "index.js"), "export * from './esm/index.js';");
-
-// An entry file for ESM
-writeFileSync(join(dist, "index.cjs.js"), "module.exports = require('./cjs/index.cjs.js');");
