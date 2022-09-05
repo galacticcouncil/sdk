e433921e3e6ca345be69734abbe7144a40c07c40
Pavol Noha <pavol.noha@gmail.com>
2022-09-05 12:16:53 +0200

    Rollback to esbuild-plugin-wasm

--- esbuild.mjs
+++ esbuild.mjs
@@ -3,43 +3,11 @@ import { wasmLoader } from 'esbuild-plugin-wasm';
 import { existsSync, mkdirSync } from 'fs';
 import { join } from 'path';
 
-import path from 'path';
-import fs from 'fs';
-
 const dist = join(process.cwd(), 'dist');
 
 if (!existsSync(dist)) {
   mkdirSync(dist);
 }
-
-const wasmLoader2 = {
-  name: 'wasm-loader',
-  setup(build) {
-    build.onResolve({ filter: /\.wasm$/ }, (args) => {
-      if (args.resolveDir === '') {
-        return;
-      }
-
-      return {
-        path: path.isAbsolute(args.path)
-          ? args.path
-          : path.join(args.resolveDir, args.path),
-        namespace: 'wasm-binary',
-      };
-    });
-
-    build.onLoad({ filter: /.*/, namespace: 'wasm-binary' }, async (args) => ({
-      contents: await fs.promises.readFile(args.path),
-      loader: 'binary',
-    }));
-
-    build.onLoad({ filter: /.*/, namespace: 'wasm-stub' }, async (args) => ({
-      contents: `import wasm from ${JSON.stringify(args.path)}
-      export default async (imports) =>
-        (await WebAssembly.instantiate(wasm, imports)).instance.exports`,
-    }));
-  },
-};
 
 // ESM bundle
 esbuild
