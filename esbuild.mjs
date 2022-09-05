89b7195254d3035f95b18209c714bc5d56837696
Pavol Noha <pavol.noha@gmail.com>
2022-09-05 12:14:08 +0200

    Wasm loader plugin example

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,14 +1,45 @@
 import esbuild from 'esbuild';
 import { wasmLoader } from 'esbuild-plugin-wasm';
-
 import { existsSync, mkdirSync } from 'fs';
 import { join } from 'path';
+
+import path from 'path';
+import fs from 'fs';
 
 const dist = join(process.cwd(), 'dist');
 
 if (!existsSync(dist)) {
   mkdirSync(dist);
 }
+
+const wasmLoader2 = {
+  name: 'wasm-loader',
+  setup(build) {
+    build.onResolve({ filter: /\.wasm$/ }, (args) => {
+      if (args.resolveDir === '') {
+        return;
+      }
+
+      return {
+        path: path.isAbsolute(args.path)
+          ? args.path
+          : path.join(args.resolveDir, args.path),
+        namespace: 'wasm-binary',
+      };
+    });
+
+    build.onLoad({ filter: /.*/, namespace: 'wasm-binary' }, async (args) => ({
+      contents: await fs.promises.readFile(args.path),
+      loader: 'binary',
+    }));
+
+    build.onLoad({ filter: /.*/, namespace: 'wasm-stub' }, async (args) => ({
+      contents: `import wasm from ${JSON.stringify(args.path)}
+      export default async (imports) =>
+        (await WebAssembly.instantiate(wasm, imports)).instance.exports`,
+    }));
+  },
+};
 
 // ESM bundle
 esbuild
@@ -17,7 +48,7 @@ esbuild
     outfile: 'dist/index.esm.js',
     bundle: true,
     sourcemap: true,
-    minify: true,
+    minify: false,
     plugins: [wasmLoader({ mode: 'embedded' })],
     format: 'esm',
     platform: 'browser',
