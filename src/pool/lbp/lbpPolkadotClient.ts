22f122bd2356b4a722f69c1539bd72d65814d61a
Pavol Noha <pavol.noha@gmail.com>
2022-09-18 08:09:25 +0200

    LBP polkadot client

---
+++ src/pool/lbp/lbpPolkadotClient.ts
@@ -0,0 +1,60 @@
+import type { StorageKey } from '@polkadot/types';
+import type { AnyTuple, Codec } from '@polkadot/types/types';
+import { PolkadotClient } from '../../client';
+import { PoolBase, PoolType } from '../../types';
+import { bnum, scale } from '../../utils/bignumber';
+import { WeightedPoolToken } from './lbpPool';
+import math from './lbpMath';
+
+interface LbpPoolData {
+  readonly assets: string[];
+  readonly feeCollector: string;
+  readonly fee: number[];
+  readonly repayTarget: number;
+  readonly initialWeight: number;
+  readonly finalWeight: number;
+  readonly start: number;
+  readonly end: number;
+}
+
+export class LbpPolkadotClient extends PolkadotClient {
+  private readonly MAX_FINAL_WEIGHT = scale(bnum(100), 6);
+
+  async getPools(): Promise<PoolBase[]> {
+    const poolAssets = await this.api.query.lbp.poolData.entries();
+    const pools = poolAssets.map(async (asset: [StorageKey<AnyTuple>, Codec]) => {
+      const poolAddress = this.getStorageKey(asset, 0);
+      const poolEntry = asset[1].toJSON() as unknown as LbpPoolData;
+      const poolTokens = await this.getPoolTokens(poolAddress, poolEntry.assets);
+      const linearWeight = await this.getLinearWeight(poolEntry);
+      const assetAWeight = bnum(linearWeight);
+      const assetBWeight = this.MAX_FINAL_WEIGHT.minus(bnum(assetAWeight));
+      return {
+        address: poolAddress,
+        type: PoolType.LBP,
+        tradeFee: this.getTradeFee(poolEntry.fee),
+        tokens: [
+          { ...poolTokens[0], weight: assetAWeight } as WeightedPoolToken,
+          { ...poolTokens[1], weight: assetBWeight } as WeightedPoolToken,
+        ],
+      } as PoolBase;
+    });
+    return Promise.all(pools);
+  }
+
+  getTradeFee([numerator, denominator]: number[]): string {
+    const res = bnum(numerator).div(bnum(denominator));
+    return res.multipliedBy(100).toString();
+  }
+
+  async getLinearWeight(poolEntry: LbpPoolData): Promise<string> {
+    const currentBlock = await this.api.query.system.number();
+    return math.calculateLinearWeights(
+      poolEntry.start.toString(),
+      poolEntry.end.toString(),
+      poolEntry.initialWeight.toString(),
+      poolEntry.finalWeight.toString(),
+      currentBlock.toString()
+    );
+  }
+}
