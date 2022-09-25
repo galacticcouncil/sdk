17f110a9c718927c82fab1370f014077b7be1dc9
Pavol Noha <pavol.noha@gmail.com>
2022-09-25 06:43:15 +0200

    Fix failing build

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { Router, TradeRouter } from './api';
-export { PolkadotPoolService, PoolFactory, XykPool } from './pool';
+export { PolkadotApiPoolService, PoolFactory, XykPool } from './pool';
 export * from './types';
 export * from './utils/bignumber';
