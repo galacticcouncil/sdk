3688c3a57ee6e86c8bf9655af6e2745450e8fe64
Pavol Noha <pavol.noha@gmail.com>
2022-12-04 18:03:35 +0000

    Export Omni & LBP

--- src/pool/index.ts
+++ src/pool/index.ts
@@ -1,3 +1,5 @@
 export { PolkadotApiPoolService } from './polkadotApiPoolService';
 export { PoolFactory } from './poolFactory';
 export { XykPool } from './xyk/xykPool';
+export { OmniPool } from './omni/omniPool';
+export { LbpPool } from './lbp/lbpPool';
