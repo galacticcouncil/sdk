139f667e1f4c3c76984f6548e55fc029e246a575
Pavol Noha <pavol.noha@gmail.com>
2023-02-21 11:50:12 +0100

    Examples cleanup

--- test/script/examples/wallet/balance/getTokenBalance.ts
+++ test/script/examples/wallet/balance/getTokenBalance.ts
@@ -2,6 +2,7 @@ import { ApiPromise } from '@polkadot/api';
 import { ApiUrl, PolkadotExecutor } from '../../../executor';
 import { TokenBalanceAdapter } from '../../../../../src/wallet';
 import { ChainAsset, Registry } from '../../../../../src/registry';
+import { HYDRADX_PARACHAIN_ID } from '../../../../../src/consts';
 import { firstValueFrom } from 'rxjs';
 
 class TokenBalanceAdapterExample extends PolkadotExecutor {
@@ -9,7 +10,7 @@ class TokenBalanceAdapterExample extends PolkadotExecutor {
     const registry = new Registry('polkadot');
     const adapter = new TokenBalanceAdapter(api);
 
-    const hydraDxAssets = registry.getAssets(2034);
+    const hydraDxAssets = registry.getAssets(HYDRADX_PARACHAIN_ID);
     const dai = hydraDxAssets.find((asset: ChainAsset) => asset.symbol == 'DAI');
     const ob = adapter.getObserver(dai!, '7MHE9BUBEWU88cEto6P1XNNb66foSwAZPKhfL8GHW9exnuH1');
 
