406bac66c6b4f783095ff5beecc5acf9727c2bae
Pavol Noha <pavol.noha@gmail.com>
2023-02-21 11:47:49 +0100

    Storage support

--- src/errors.ts
+++ src/errors.ts
@@ -15,3 +15,35 @@ export class RouteNotFound extends Error {
     this.name = 'RouteNotFound';
   }
 }
+
+export class AssetNotFound extends Error {
+  constructor(asset: string) {
+    super();
+    this.message = `Asset ${asset} not available in current network`;
+    this.name = 'AssetNotFound';
+  }
+}
+
+export class StorageConfigNotFound extends Error {
+  constructor(param: string) {
+    super();
+    this.message = `Storage missing ${param}`;
+    this.name = 'StorageConfigNotFound';
+  }
+}
+
+export class SubscriptionNotSupported extends Error {
+  constructor(subscritpionType: string) {
+    super();
+    this.message = `Subscription type "${subscritpionType}" not supported`;
+    this.name = 'SubscriptionNotSupported';
+  }
+}
+
+export class ProviderConfigNotFound extends Error {
+  constructor(message: string) {
+    super();
+    this.message = `${message}`;
+    this.name = 'ProviderConfigNotFound';
+  }
+}
