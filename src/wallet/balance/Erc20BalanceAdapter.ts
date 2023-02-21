c86bf31d1326045ed4618d751b1a6450c4f9cffb
Lumir Mrkva <mrq@mrq.cz>
2023-02-21 20:24:03 +0100

    using ethers instead of web3 to get balance

--- src/wallet/balance/Erc20BalanceAdapter.ts
+++ src/wallet/balance/Erc20BalanceAdapter.ts
@@ -1,38 +1,27 @@
-import Web3 from 'web3';
-import { AbiItem } from 'web3-utils';
+import { AbstractProvider, Contract, ethers } from 'ethers';
 import { Observable, Subject, shareReplay } from 'rxjs';
 import { AssetBalance } from '../../types';
 import { ChainAsset } from '../../registry';
 import { bnum, ZERO } from '../../utils/bignumber';
 import { BalanceAdapter, EvmProvider } from '../types';
 
-const minABI: AbiItem[] = [
-  {
-    constant: true,
-    inputs: [{ name: '_owner', type: 'address' }],
-    name: 'balanceOf',
-    outputs: [{ name: 'balance', type: 'uint256' }],
-    type: 'function',
-  },
-];
-
 export class Erc20BalanceAdapter implements BalanceAdapter {
-  readonly client: Web3;
+  readonly client: AbstractProvider;
   readonly evmProvider: EvmProvider;
 
   constructor(evmProvider: EvmProvider) {
     this.evmProvider = evmProvider;
-    this.client = new Web3(new Web3.providers.HttpProvider(evmProvider.getEndpoint()));
+    this.client = ethers.getDefaultProvider(evmProvider.getEndpoint());
   }
 
   public getObserver(asset: ChainAsset, address: string): Observable<AssetBalance> {
     const balance$ = new Subject<AssetBalance>();
     const observer = balance$.pipe(shareReplay(1));
-    const contract = new this.client.eth.Contract(minABI, asset.asset.Erc20);
+    const token = new Contract(asset.asset.Erc20, ["function balanceOf(address) view returns (uint256)"], this.client);
 
     const run = async () => {
       const evmAddress = await this.evmProvider.toEvmAddress(address);
-      const balance = await contract.methods.balanceOf(evmAddress).call();
+      const balance = await token.balanceOf(evmAddress);
       balance$.next({
         free: bnum(balance),
         locked: ZERO,
