108e24e937136c911030b64e04f34b05f8b5eade
Alexander Popiak <alexander.popiak@parity.io>
2023-05-04 16:58:14 +0200

    add period constants and more tests

--- test/ema/emaMath.spec.ts
+++ test/ema/emaMath.spec.ts
@@ -5,9 +5,9 @@ function smoothingFromPeriod(period: number): BigNumber {
   return bnum(2).pow(127).multipliedBy(2).dividedBy(period + 1)
 }
 
-describe('Ema Math', () => {
+describe('EMA Math', () => {
 
-  it('Should return correct EMA for given params', async () => {
+  it('Should return correct price EMA for simple params', async () => {
     let smoothing = smoothingFromPeriod(7);
     let startPriceN = bnum(4);
     let startPriceD = bnum(1);
@@ -16,6 +16,28 @@ describe('Ema Math', () => {
     let iterations = bnum(1);
     let nextPrice = EmaLowPrecisionMath.iteratedPriceEma(iterations.toString(), startPriceN.toString(), startPriceD.toString(), incomingPriceN.toString(), incomingPriceD.toString(), smoothing.toString());
     let expected = bnum(5).multipliedBy(bnum("1000000000000000000"));
+    expect(bnum(nextPrice)).toStrictEqual(expected);
+  });
+
+  it('Should return correct price EMA for hard-coded period', async () => {
+    let smoothing = EmaLowPrecisionMath.Short;
+    let startPriceN = bnum("100");
+    let startPriceD = bnum(1);
+    let incomingPriceN = bnum("1100");
+    let incomingPriceD = bnum(1);
+    let iterations = bnum(1);
+    let nextPrice = EmaLowPrecisionMath.iteratedPriceEma(iterations.toString(), startPriceN.toString(), startPriceD.toString(), incomingPriceN.toString(), incomingPriceD.toString(), smoothing.toString());
+    let expected = bnum("300").multipliedBy(bnum("1000000000000000000"));
+    expect(bnum(nextPrice)).toStrictEqual(expected);
+  });
+
+  it('Should return correct balance EMA for hard-coded period', async () => {
+    let smoothing = EmaLowPrecisionMath.Short;
+    let startBalance = bnum("100000000");
+    let incomingBalance = bnum("1100000000");
+    let iterations = bnum(1);
+    let nextPrice = EmaLowPrecisionMath.iteratedBalanceEma(iterations.toString(), startBalance.toString(), incomingBalance.toString(), smoothing.toString());
+    let expected = bnum("300000000");
     expect(bnum(nextPrice)).toStrictEqual(expected);
   });
 });
