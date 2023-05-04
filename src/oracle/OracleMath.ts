24bca1ab5c8622bbbef8507b24de5efefa938b08
Alexander Popiak <alexander.popiak@parity.io>
2023-05-04 17:47:51 +0200

    add convenience abstraction over low level oracle math

--- src/oracle/OracleMath.ts
+++ src/oracle/OracleMath.ts
@@ -1,4 +1,22 @@
 import { low_precision_iterated_price_ema, iterated_balance_ema } from '@galacticcouncil/math-ema';
+import BigNumber from 'bignumber.js';
+import { ZERO, bnum } from '../utils/bignumber';
+
+interface OracleEntry {
+  price: [BigNumber, BigNumber];
+  volume: BigNumber[];
+  liquidity: [BigNumber, BigNumber];
+  timestamp: BigNumber;
+}
+
+interface LowPrecisionOracleEntry {
+  price: BigNumber;
+  volume: BigNumber[];
+  liquidity: [BigNumber, BigNumber];
+  timestamp: BigNumber;
+}
+
+type OraclePeriod = 'LastBlock' | 'Short' | 'TenMinutes' | 'Hour' | 'Day' | 'Week';
 
 export class EmaLowPrecisionMath {
   // Smoothing factors for the currently supported oracle periods.
@@ -22,5 +40,53 @@ export class EmaLowPrecisionMath {
 
   static iteratedBalanceEma(iterations: string, prev: string, incoming: string, smoothing: string): string {
     return iterated_balance_ema(iterations, prev, incoming, smoothing);
+  }
+}
+
+export class OracleMath {
+  static readonly SmoothingForPeriod: Map<string, string> = new Map([
+    ['LastBlock', EmaLowPrecisionMath.LastBlock],
+    ['Short', EmaLowPrecisionMath.Short],
+    ['TenMinutes', EmaLowPrecisionMath.TenMinutes],
+    ['Hour', EmaLowPrecisionMath.Hour],
+    ['Day', EmaLowPrecisionMath.Day],
+    ['Week', EmaLowPrecisionMath.Week],
+  ]);
+
+  /// Calculate the current oracle values from the `outdated` and `update_with` values using the `smoothing` factor with the old values being `iterations` out of date.
+  ///
+  /// Note: The volume is always updated with zero values so it is not a parameter.
+  static updateOutdatedToCurrent(
+    outdated: OracleEntry,
+    updateWith: OracleEntry,
+    period: OraclePeriod
+  ): LowPrecisionOracleEntry {
+    if (outdated.timestamp > updateWith.timestamp) {
+      throw new Error('invalid timestamp');
+    }
+    let iterations = BigNumber.max(updateWith.timestamp.minus(outdated.timestamp), 0).toString();
+    let smoothing = OracleMath.SmoothingForPeriod[period];
+    let [prevN, prevD] = outdated.price;
+    let [incomingN, incomingD] = updateWith.price;
+    let price = bnum(
+      EmaLowPrecisionMath.iteratedPriceEma(
+        iterations,
+        prevN.toString(),
+        prevD.toString(),
+        incomingN.toString(),
+        incomingD.toString(),
+        smoothing
+      )
+    );
+    let volume = outdated.volume.map((v) =>
+      bnum(EmaLowPrecisionMath.iteratedBalanceEma(iterations, v.toString(), ZERO.toString(), smoothing))
+    );
+    let [prevLiq1, prevLiq2] = outdated.liquidity;
+    let [incomingLiq1, incomingLiq2] = updateWith.liquidity;
+    let liquidity: [BigNumber, BigNumber] = [
+      bnum(EmaLowPrecisionMath.iteratedBalanceEma(iterations, prevLiq1.toString(), incomingLiq1.toString(), smoothing)),
+      bnum(EmaLowPrecisionMath.iteratedBalanceEma(iterations, prevLiq2.toString(), incomingLiq2.toString(), smoothing)),
+    ];
+    return { price, volume, liquidity, timestamp: updateWith.timestamp };
   }
 }
