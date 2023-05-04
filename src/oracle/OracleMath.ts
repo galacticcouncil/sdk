228eaafef770979348efc721a44d72ec81f9aede
Alexander Popiak <alexander.popiak@parity.io>
2023-05-04 17:00:54 +0200

    formatting

--- src/oracle/OracleMath.ts
+++ src/oracle/OracleMath.ts
@@ -1,18 +1,22 @@
-import {
-  low_precision_iterated_price_ema,
-  iterated_balance_ema,
-} from '@galacticcouncil/math-ema';
+import { low_precision_iterated_price_ema, iterated_balance_ema } from '@galacticcouncil/math-ema';
 
 export class EmaLowPrecisionMath {
   // Smoothing factors for the currently supported oracle periods.
-  static readonly LastBlock: string = "170141183460469231731687303715884105728";
-  static readonly Short: string = "34028236692093846346337460743176821146";
-  static readonly TenMinutes: string = "3369132345751865974884897103284833777";
-  static readonly Hour: string = "566193622164623067326746434994622648";
-  static readonly Day: string = "23629079016800115510268356880200556";
-  static readonly Week: string = "3375783642235081630771268215908257";
+  static readonly LastBlock: string = '170141183460469231731687303715884105728';
+  static readonly Short: string = '34028236692093846346337460743176821146';
+  static readonly TenMinutes: string = '3369132345751865974884897103284833777';
+  static readonly Hour: string = '566193622164623067326746434994622648';
+  static readonly Day: string = '23629079016800115510268356880200556';
+  static readonly Week: string = '3375783642235081630771268215908257';
 
-  static iteratedPriceEma(iterations: string, prev_n: string, prev_d: string, incoming_n: string, incoming_d: string, smoothing: string): string {
+  static iteratedPriceEma(
+    iterations: string,
+    prev_n: string,
+    prev_d: string,
+    incoming_n: string,
+    incoming_d: string,
+    smoothing: string
+  ): string {
     return low_precision_iterated_price_ema(iterations, prev_n, prev_d, incoming_n, incoming_d, smoothing);
   }
 
--- test/ema/emaMath.spec.ts
+++ test/ema/emaMath.spec.ts
@@ -2,11 +2,13 @@ import { EmaLowPrecisionMath } from '../../src/oracle';
 import { BigNumber, bnum } from '../../src/utils/bignumber';
 
 function smoothingFromPeriod(period: number): BigNumber {
-  return bnum(2).pow(127).multipliedBy(2).dividedBy(period + 1)
+  return bnum(2)
+    .pow(127)
+    .multipliedBy(2)
+    .dividedBy(period + 1);
 }
 
 describe('EMA Math', () => {
-
   it('Should return correct price EMA for simple params', async () => {
     let smoothing = smoothingFromPeriod(7);
     let startPriceN = bnum(4);
@@ -14,30 +16,49 @@ describe('EMA Math', () => {
     let incomingPriceN = bnum(8);
     let incomingPriceD = bnum(1);
     let iterations = bnum(1);
-    let nextPrice = EmaLowPrecisionMath.iteratedPriceEma(iterations.toString(), startPriceN.toString(), startPriceD.toString(), incomingPriceN.toString(), incomingPriceD.toString(), smoothing.toString());
-    let expected = bnum(5).multipliedBy(bnum("1000000000000000000"));
+    let nextPrice = EmaLowPrecisionMath.iteratedPriceEma(
+      iterations.toString(),
+      startPriceN.toString(),
+      startPriceD.toString(),
+      incomingPriceN.toString(),
+      incomingPriceD.toString(),
+      smoothing.toString()
+    );
+    let expected = bnum(5).multipliedBy(bnum('1000000000000000000'));
     expect(bnum(nextPrice)).toStrictEqual(expected);
   });
 
   it('Should return correct price EMA for hard-coded period', async () => {
     let smoothing = EmaLowPrecisionMath.Short;
-    let startPriceN = bnum("100");
+    let startPriceN = bnum('100');
     let startPriceD = bnum(1);
-    let incomingPriceN = bnum("1100");
+    let incomingPriceN = bnum('1100');
     let incomingPriceD = bnum(1);
     let iterations = bnum(1);
-    let nextPrice = EmaLowPrecisionMath.iteratedPriceEma(iterations.toString(), startPriceN.toString(), startPriceD.toString(), incomingPriceN.toString(), incomingPriceD.toString(), smoothing.toString());
-    let expected = bnum("300").multipliedBy(bnum("1000000000000000000"));
+    let nextPrice = EmaLowPrecisionMath.iteratedPriceEma(
+      iterations.toString(),
+      startPriceN.toString(),
+      startPriceD.toString(),
+      incomingPriceN.toString(),
+      incomingPriceD.toString(),
+      smoothing.toString()
+    );
+    let expected = bnum('300').multipliedBy(bnum('1000000000000000000'));
     expect(bnum(nextPrice)).toStrictEqual(expected);
   });
 
   it('Should return correct balance EMA for hard-coded period', async () => {
     let smoothing = EmaLowPrecisionMath.Short;
-    let startBalance = bnum("100000000");
-    let incomingBalance = bnum("1100000000");
+    let startBalance = bnum('100000000');
+    let incomingBalance = bnum('1100000000');
     let iterations = bnum(1);
-    let nextPrice = EmaLowPrecisionMath.iteratedBalanceEma(iterations.toString(), startBalance.toString(), incomingBalance.toString(), smoothing.toString());
-    let expected = bnum("300000000");
+    let nextPrice = EmaLowPrecisionMath.iteratedBalanceEma(
+      iterations.toString(),
+      startBalance.toString(),
+      incomingBalance.toString(),
+      smoothing.toString()
+    );
+    let expected = bnum('300000000');
     expect(bnum(nextPrice)).toStrictEqual(expected);
   });
 });
