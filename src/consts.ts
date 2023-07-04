613208fac6e1948edcb649dd3a9807f943e70fd3
Pavol Noha <pavol.noha@gmail.com>
2023-07-04 12:26:33 +0200

    Kusama token balance script

--- src/consts.ts
+++ src/consts.ts
@@ -6,4 +6,6 @@ export const SYSTEM_ASSET_DECIMALS = 12;
 export const HYDRADX_PARACHAIN_ID = 2034;
 export const HYDRADX_SS58_PREFIX = 63;
 
+export const BASILISK_PARACHAIN_ID = 2090;
+
 export const DENOMINATOR = 1000;
---
+++ test/script/examples/wallet/balance/kusama/getTokenBalance.ts
@@ -0,0 +1,27 @@
+import { ApiPromise } from '@polkadot/api';
+import { ApiUrl, PolkadotExecutor } from '../../../../executor';
+import { TokenBalanceAdapter } from '../../../../../../src/wallet';
+import { ChainAsset, KusamaRegistry } from '../../../../../../src/registry';
+import { BASILISK_PARACHAIN_ID } from '../../../../../../src/consts';
+import { firstValueFrom } from 'rxjs';
+
+class TokenBalanceAdapterExample extends PolkadotExecutor {
+  async script(api: ApiPromise): Promise<any> {
+    const assetSymbol = 'USDT';
+    const chainId = BASILISK_PARACHAIN_ID;
+
+    const registry = new KusamaRegistry();
+    const adapter = new TokenBalanceAdapter(api);
+
+    const chainAssets = registry.getAssets(chainId);
+    console.log(chainAssets);
+    const chainAsset = chainAssets.find((asset: ChainAsset) => asset.symbol == assetSymbol);
+    console.log(chainAsset);
+
+    const ob = adapter.getObserver(chainAsset!, 'Ed6e66kkcbjEPtpxk7JHTTRJx2xhm4Yka3pE2BuJRRpo9jp');
+
+    return firstValueFrom(ob);
+  }
+}
+
+new TokenBalanceAdapterExample(ApiUrl.Basilisk, 'Get balance', true).run();
