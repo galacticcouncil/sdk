b242399345f8410577f1ffe9dcaa8c4930e8b21e
mrq <mrq@mrq.cz>
2023-10-24 21:09:55 +0200

    with LBP

--- packages/sdk/test/script/examples/router/timeSpotPrice.ts
+++ packages/sdk/test/script/examples/router/timeSpotPrice.ts
@@ -7,13 +7,13 @@ import {PoolType} from '../../../../src/types';
 class GetBestSpotPriceExample extends PolkadotExecutor {
   async script(api: ApiPromise): Promise<any> {
     const poolService = new PoolService(api);
-    const router = new TradeRouter(poolService, { includeOnly: [PoolType.Omni, PoolType.Stable] });
+    const router = new TradeRouter(poolService, { includeOnly: [PoolType.Omni, PoolType.Stable, PoolType.LBP] });
     let result;
-    console.time('Get best spot price with LBP 10x')
+    console.time('spot price 10x')
     for (let i = 0; i < 10; i++) {
       result = await router.getBestSpotPrice('5', '2');
     }
-    console.timeEnd('Get best spot price with LBP 10x')
+    console.timeEnd('spot price 10x')
     return result;
   }
 }
