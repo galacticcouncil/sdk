6f9229323c049fd59e3192e62ee3174353cc498e
Pavol Noha <pavol.noha@gmail.com>
2024-04-14 12:30:13 +0200

    Fix sdk dev build

--- packages/sdk/esbuild.dev.mjs
+++ packages/sdk/esbuild.dev.mjs
@@ -2,14 +2,19 @@ import esbuild from 'esbuild';
 import { esmConfig, getPackageJson } from '../../esbuild.config.mjs';
 
 const packageJson = getPackageJson(import.meta.url);
-const peerDependencies = packageJson.peerDependencies || {};
+const peerDependencies = Object.keys(packageJson.peerDependencies);
+const dependencies = Object.keys(packageJson.dependencies);
+
+const mathDependencies = dependencies.filter((v) =>
+  v.startsWith('@galacticcouncil/math-')
+);
 
 const plugins = [];
 const options = {
   ...esmConfig,
   bundle: true,
   sourcemap: true,
-  external: Object.keys(peerDependencies),
+  external: Object.keys(peerDependencies).concat(mathDependencies),
 };
 
 const ctx = await esbuild.context({ ...options, plugins });
