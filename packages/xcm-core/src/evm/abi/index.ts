9bc1377eb19cf3bed307193219c8f8e977fbe31a
Pavol Noha <pavol.noha@gmail.com>
2024-05-21 21:55:17 -0600

    update core abis

--- packages/xcm-core/src/evm/abi/index.ts
+++ packages/xcm-core/src/evm/abi/index.ts
@@ -1,14 +1,18 @@
 import { BATCH } from './Batch';
+import { BRIDGE } from './Bridge';
 import { GMP } from './Gmp';
 import { ERC20 } from './Erc20';
+import { META } from './Meta';
 import { TOKEN_BRIDGE } from './TokenBridge';
 import { TOKEN_RELAYER } from './TokenRelayer';
 import { XTOKENS } from './Xtokens';
 
 export const Abi: Record<string, unknown[]> = {
   Batch: BATCH,
+  Bridge: BRIDGE,
   Gmp: GMP,
   Erc20: ERC20,
+  Meta: META,
   TokenBridge: TOKEN_BRIDGE,
   TokenRelayer: TOKEN_RELAYER,
   Xtokens: XTOKENS,
