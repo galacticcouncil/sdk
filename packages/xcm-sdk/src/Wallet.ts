fb5ad4183c5d0182791aafdc4c2f03af7f288c11
Pavol Noha <pavol.noha@gmail.com>
2024-09-24 10:49:45 -0600

    Fix min amount calc

--- packages/xcm-sdk/src/Wallet.ts
+++ packages/xcm-sdk/src/Wallet.ts
@@ -69,7 +69,12 @@ export class Wallet {
     );
 
     const dstEd = await dst.metadata.getEd();
-    const min = calculateMin(dstBalance, dstFee, dstMin, dstEd);
+    const min = calculateMin(
+      srcBalance.copyWith({ amount: dstBalance.amount }),
+      dstFee,
+      dstMin,
+      dstEd
+    );
 
     const srcEd = await src.metadata.getEd();
     const max = calculateMax(srcBalance, srcFee, srcMin, srcEd);
