3072aefea870f13803f0c9653c9f9ac1e35fe166
Pavol Noha <pavol.noha@gmail.com>
2024-10-25 18:09:40 +0200

    cleanup xmc-test

--- packages/xcm-test/src/utils.ts
+++ packages/xcm-test/src/utils.ts
@@ -1,7 +1,43 @@
-import { Asset, AssetAmount } from '@galacticcouncil/xcm-core';
+import {
+  AnyChain,
+  Asset,
+  AssetAmount,
+  AssetRoute,
+  Parachain,
+} from '@galacticcouncil/xcm-core';
 import { Metadata } from '@galacticcouncil/xcm-sdk';
 
-export const buildAmount = async (
+const SS85_ADDRESS = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
+const H160_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+const isfullAddressSpace = (chain: AnyChain): boolean => {
+  return (
+    chain instanceof Parachain &&
+    chain.isEvmParachain() &&
+    chain.usesH160Acc == false
+  );
+};
+
+const ish160AddressSpaceOnly = (chain: AnyChain): boolean => {
+  return (
+    (chain instanceof Parachain && chain.usesH160Acc == true) ||
+    chain.isEvmChain()
+  );
+};
+
+export const getAddress = (chain: AnyChain): string => {
+  if (isfullAddressSpace(chain)) {
+    return SS85_ADDRESS;
+  }
+
+  if (ish160AddressSpaceOnly(chain)) {
+    return H160_ADDRESS;
+  }
+
+  return SS85_ADDRESS;
+};
+
+export const getAmount = async (
   amount: number,
   asset: Asset,
   metadata: Metadata
@@ -12,4 +48,9 @@ export const buildAmount = async (
     decimals: assetDecimals,
     amount: BigInt(assetAmount),
   });
+};
+
+export const getRouteKey = (chain: AnyChain, route: AssetRoute) => {
+  const { source, destination } = route;
+  return [chain.key, destination.chain.key, source.asset.key].join('-');
 };
--- packages/xcm-test/src/wallet.spec.ts
+++ packages/xcm-test/src/wallet.spec.ts
@@ -5,7 +5,6 @@ import {
   Asset,
   AssetRoute,
   ChainEcosystem,
-  ChainRoutes,
   Parachain,
   SubstrateApis,
 } from '@galacticcouncil/xcm-core';
@@ -21,175 +20,15 @@ import console from 'console';
 import outdent from 'outdent';
 
 import { configService, init } from './wallet';
-import { buildAmount } from './utils';
+import { getAddress, getAmount, getRouteKey } from './utils';
 
-import hydration from './data/hydration.json';
-
-import snp from './data/snapshot.json';
-
-interface Snapshot {
-  [key: string]: any;
-}
-
-const SNAPSHOTS: Snapshot = {
-  hydration: hydration,
-};
+import snapshotJson from './snapshot.json';
 
 const jestConsole = console;
 
 const BALANCE = 10;
 const FEE = 0.1;
 const FEE_BALANCE = 1;
-
-const testCase = async (
-  wallet: Wallet,
-  snapshot: any,
-  chainKey: string,
-  sender: string
-) => {
-  const config = wallet.config.getChainRoutes(chainKey);
-  const { chain, routes } = config;
-
-  const result = new Map();
-  const snaps = new Map(Object.entries(snapshot));
-
-  for (const [key, route] of routes.entries()) {
-    const { source, destination } = route;
-    const sourceMeta = new Metadata(chain);
-    const destinationMeta = new Metadata(destination.chain);
-
-    const getBalanceMock = jest
-      .spyOn(TransferService.prototype, 'getBalance')
-      .mockImplementation(async (address) => {
-        if (address === sender) {
-          return buildAmount(BALANCE, source.asset, sourceMeta);
-        }
-        return buildAmount(BALANCE, destination.asset, destinationMeta);
-      });
-
-    const getFeeBalanceMock = jest
-      .spyOn(TransferService.prototype, 'getFeeBalance')
-      .mockImplementation(async () => {
-        if (source.fee) {
-          const feeAsset = source.fee.asset;
-          const asset: Asset =
-            'build' in feeAsset
-              ? await feeAsset
-                  .build({
-                    chain: chain as Parachain,
-                    address: sender,
-                  })
-                  .call()
-              : feeAsset;
-          return buildAmount(FEE, asset, sourceMeta);
-        }
-        return buildAmount(FEE, source.asset, sourceMeta);
-      });
-
-    const getDestinationFeeBalanceMock = jest
-      .spyOn(TransferService.prototype, 'getDestinationFeeBalance')
-      .mockImplementation(async () => {
-        return buildAmount(FEE_BALANCE, destination.fee.asset, destinationMeta);
-      });
-
-    const getDestinationFeeMock = jest
-      .spyOn(TransferService.prototype, 'getDestinationFee')
-      .mockImplementation(async () => {
-        return buildAmount(FEE, destination.fee.asset, destinationMeta);
-      });
-
-    const getFeeMock = jest
-      .spyOn(TransferService.prototype, 'getFee')
-      .mockImplementation(async () => {
-        return buildAmount(FEE, source.asset, sourceMeta);
-      });
-
-    const isSwapSupportedMock = jest
-      .spyOn(wallet.dex, 'isSwapSupported')
-      .mockImplementation(() => false);
-
-    const xTransfer = await wallet.transfer(
-      source.asset,
-      sender,
-      chain,
-      destination.chain.isEvm()
-        ? '0x0000000000000000000000000000000000000000'
-        : sender,
-      destination.chain
-    );
-
-    expect(getBalanceMock).toHaveBeenCalled();
-    expect(getFeeBalanceMock).toHaveBeenCalled();
-    expect(getDestinationFeeBalanceMock).toHaveBeenCalled();
-    expect(getDestinationFeeMock).toHaveBeenCalled();
-    expect(getFeeMock).toHaveBeenCalled();
-    expect(isSwapSupportedMock).toHaveBeenCalled();
-
-    getBalanceMock.mockClear();
-    getFeeBalanceMock.mockClear();
-    getDestinationFeeBalanceMock.mockClear();
-    getDestinationFeeMock.mockClear();
-    getFeeMock.mockClear();
-    isSwapSupportedMock.mockClear();
-
-    const calldata = await xTransfer.buildCall('1');
-
-    const data = snaps.get(key);
-    if (data) {
-      // Fail fast if snapshot mismatch
-      expect(calldata.data).toStrictEqual(data);
-      const message = outdent`
-        Route: ${chain.name} -> ${destination.chain.name} [ ${source.asset.originSymbol} ]
-        Call: ${calldata.data}
-        Status: ✅ \n
-      `;
-      console.log(message);
-    } else {
-      const message = outdent`
-        Route: ${chain.name} -> ${destination.chain.name} [ ${source.asset.originSymbol} ]
-        Call: ${calldata.data}
-        Status: ❗ Unknown transfer. Queued for processing. \n
-      `;
-      console.log(message);
-    }
-    result.set(key, calldata.data);
-  }
-  const json = JSON.stringify(Object.fromEntries(result), null, 2);
-  const file = ['./src/data/', chainKey, '.json'].join('');
-  writeFileSync(file, json);
-};
-
-const fullAddressSpace = (chain: AnyChain): boolean => {
-  return (
-    chain instanceof Parachain &&
-    chain.isEvmParachain() &&
-    chain.usesH160Acc == false
-  );
-};
-
-const h160AddressSpaceOnly = (chain: AnyChain): boolean => {
-  return (
-    (chain instanceof Parachain && chain.usesH160Acc == true) ||
-    chain.isEvmChain()
-  );
-};
-
-const getAddress = (chain: AnyChain): string => {
-  if (fullAddressSpace(chain)) {
-    return '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
-  }
-
-  if (h160AddressSpaceOnly(chain)) {
-    return '0x0000000000000000000000000000000000000000';
-  }
-
-  return '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
-};
-
-const buildKey = (chain: AnyChain, route: AssetRoute) => {
-  const { source, destination } = route;
-  return [chain.key, destination.chain.key, source.asset.key].join('-');
-};
 
 const getCalldata = async (
   wallet: Wallet,
@@ -206,7 +45,7 @@ const getCalldata = async (
   const getBalanceMock = jest
     .spyOn(TransferService.prototype, 'getBalance')
     .mockImplementation(async () =>
-      buildAmount(BALANCE, source.asset, sourceMeta)
+      getAmount(BALANCE, source.asset, sourceMeta)
     );
 
   const getFeeBalanceMock = jest
@@ -223,27 +62,27 @@ const getCalldata = async (
                 })
                 .call()
             : feeAsset;
-        return buildAmount(FEE, asset, sourceMeta);
+        return getAmount(FEE, asset, sourceMeta);
       }
-      return buildAmount(FEE, source.asset, sourceMeta);
+      return getAmount(FEE, source.asset, sourceMeta);
     });
 
   const getDestinationFeeBalanceMock = jest
     .spyOn(TransferService.prototype, 'getDestinationFeeBalance')
     .mockImplementation(async () => {
-      return buildAmount(FEE_BALANCE, destination.fee.asset, destinationMeta);
+      return getAmount(FEE_BALANCE, destination.fee.asset, destinationMeta);
     });
 
   const getDestinationFeeMock = jest
     .spyOn(TransferService.prototype, 'getDestinationFee')
     .mockImplementation(async () => {
-      return buildAmount(FEE, destination.fee.asset, destinationMeta);
+      return getAmount(FEE, destination.fee.asset, destinationMeta);
     });
 
   const getFeeMock = jest
     .spyOn(TransferService.prototype, 'getFee')
     .mockImplementation(async () => {
-      return buildAmount(FEE, source.asset, sourceMeta);
+      return getAmount(FEE, source.asset, sourceMeta);
     });
 
   const isSwapSupportedMock = jest
@@ -279,11 +118,11 @@ const getCalldata = async (
 };
 
 describe('Wallet', () => {
-  jest.setTimeout(2 * 60 * 1000); // Execution time 2min
+  jest.setTimeout(2 * 60 * 1000); // Max execution time 2 min
 
   let wallet: Wallet;
 
-  const snapshot = new Map(Object.entries(snp));
+  const snapshot = new Map(Object.entries(snapshotJson));
   const report = new Map();
   const chains = Array.from(configService.chains.values())
     .filter(
@@ -304,9 +143,9 @@ describe('Wallet', () => {
     SubstrateApis.getInstance().release();
 
     if (snapshot.size === 0) {
-      // Init snapshot from report
+      // Initialize snapshot from report on boot
       const json = JSON.stringify(Object.fromEntries(report), null, 2);
-      const file = ['./src/data/snapshot.json'].join('');
+      const file = ['./src/snapshot.json'].join('');
       writeFileSync(file, json);
     }
   });
@@ -330,7 +169,7 @@ describe('Wallet', () => {
         const transferTo = destination.chain.name;
         const transferAsset = source.asset.originSymbol;
 
-        const key = buildKey(chain, route);
+        const key = getRouteKey(chain, route);
         const calldata = snapshot.get(key);
 
         it(`${transferFrom} -> ${transferTo} [ ${transferAsset} ] transfer`, async () => {
