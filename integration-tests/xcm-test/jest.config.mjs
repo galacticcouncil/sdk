933945b5fcbe49cd3e407be5ea19359e56d3b4a8
Pavol Noha <pavol.noha@gmail.com>
2024-12-02 16:38:54 +0100

    chopsticks env variables

--- integration-tests/xcm-test/jest.config.mjs
+++ integration-tests/xcm-test/jest.config.mjs
@@ -1,3 +1,19 @@
 import { config } from '../../jest.config.mjs';
 
+/**
+ * Chopsticks log level, "fatal" | "error" | "warn" | "info" | "debug" | "trace".
+ * Default is "info".
+ */
+process.env.LOG_LEVEL = 'info';
+
+/**
+ * Don't log objects. Default is `false`.
+ */
+process.env.LOG_COMPACT = 'false';
+
+/**
+ * Don't truncate log messages, show full log output. Default is `false`.
+ */
+process.env.VERBOSE_LOG = 'false';
+
 export default config;
