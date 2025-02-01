8dfc614e1978816e35c11dffcb86e84479ad6ec9
Pavol Noha <pavol.noha@gmail.com>
2025-02-01 21:21:11 +0200

    extrinsic tx options #118

--- packages/xcm-core/src/config/definition/extrinsic/ExtrinsicConfig.ts
+++ packages/xcm-core/src/config/definition/extrinsic/ExtrinsicConfig.ts
@@ -3,14 +3,20 @@ import { SubmittableExtrinsicFunction } from '@polkadot/api/types';
 import { BaseConfig, BaseConfigParams, CallType } from '../base';
 
 export interface ExtrinsicConfigParams extends Omit<BaseConfigParams, 'type'> {
+  txOptions?: {
+    asset?: any;
+  };
   getArgs: (func?: SubmittableExtrinsicFunction<'promise'>) => any[];
 }
 
 export class ExtrinsicConfig extends BaseConfig {
   getArgs: (func?: SubmittableExtrinsicFunction<'promise'>) => any[];
 
-  constructor({ getArgs, ...other }: ExtrinsicConfigParams) {
+  readonly txOptions?: any;
+
+  constructor({ getArgs, txOptions, ...other }: ExtrinsicConfigParams) {
     super({ ...other, type: CallType.Substrate });
     this.getArgs = getArgs;
+    this.txOptions = txOptions;
   }
 }
