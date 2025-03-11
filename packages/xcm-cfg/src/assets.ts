996d05d1e2486d23957d162c53e2e0c0c7a19253
Noah Oliver G. Rigonan <oliverrigonan@gmail.com>
2025-03-11 11:33:18 +0800

    implementation for cross-chain integration: Basilisks and Xode

--- packages/xcm-cfg/src/assets.ts
+++ packages/xcm-cfg/src/assets.ts
@@ -265,6 +265,11 @@ export const ztg = new Asset({
   originSymbol: 'ZTG',
 });
 
+export const xon = new Asset({
+  key: 'xon',
+  originSymbol: 'XON',
+});
+
 export const assets: Asset[] = [
   aave,
   aca,
@@ -319,6 +324,7 @@ export const assets: Asset[] = [
   wud,
   xrt,
   ztg,
+  xon,
 ];
 
 export const assetsMap = new Map<string, Asset>(
