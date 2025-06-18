87a4074f0e1a7d5e10c70bf075fabdc82024b3d9
Pavol Noha <pavol.noha@gmail.com>
2025-06-18 11:52:46 +0200

    update example signers

--- examples/sdk-esm/src/index.ts
+++ examples/sdk-esm/src/index.ts
@@ -1,28 +1,47 @@
-import { createSdkContext } from '@galacticcouncil/sdk';
+import {
+  createSdkContext,
+  EvmClient,
+  SubstrateTransaction,
+} from '@galacticcouncil/sdk';
 import { ApiPromise, WsProvider } from '@polkadot/api';
 
-import { signAndSend } from './signers/evm';
+import { evm as evmSigner, substrate as substrateSigner } from './signers';
 
-const ws = 'wss://hydration-rpc.n.dwellir.com';
+const WS_ENDPOINT = 'wss://hydration-rpc.n.dwellir.com';
 const BENEFICIARY = '7L53bUTBopuwFt3mKUfmkzgGLayYa1Yvn1hAg9v5UMrQzTfh';
 
-const wsProvider = new WsProvider(ws, 2_500, {}, 60_000, 102400, 10 * 60_000);
+// Initialize conn & create sdk context
+
+const wsProvider = new WsProvider(
+  WS_ENDPOINT,
+  2_500,
+  {},
+  60_000,
+  102400,
+  10 * 60_000
+);
 const apiPromise = await ApiPromise.create({ provider: wsProvider });
 
 const sdk = createSdkContext(apiPromise);
 
 const { api, evm, tx } = sdk;
 
+// Trade
+
 const trade = await api.router.getBestSell('69', '5', '1');
 print(trade);
 
-const tradeTx = await tx.trade(trade).withBeneficiary(BENEFICIARY).build();
+const tradeTx = await tx
+  .trade(trade)
+  .withSlippage(1)
+  .withBeneficiary(BENEFICIARY)
+  .build();
 print(tradeTx.get());
 
 const { executionResult } = await tradeTx.dryRun(BENEFICIARY);
 print(executionResult);
 
-//signEvm(tradeTx.hex);
+// Cleanup
 
 setTimeout(() => {
   sdk.destroy();
@@ -30,21 +49,40 @@ setTimeout(() => {
   console.log('Unsubscribed');
 }, 60000);
 
+// Helpers
+
 function print(object: any) {
   console.log(JSON.stringify(object.toHuman(), null, 2));
 }
 
-function signEvm(call: string) {
-  signAndSend(
-    BENEFICIARY,
-    call,
-    apiPromise,
+function signWithEvm(
+  beneficiary: string,
+  evm: EvmClient,
+  tx: SubstrateTransaction
+) {
+  evmSigner.signAndSend(
+    beneficiary,
     evm,
+    tx,
     (hash) => {
       console.log('TxHash: ' + hash);
     },
     (receipt) => {
       console.log(receipt);
+    },
+    (error) => {
+      console.error(error);
+    }
+  );
+}
+
+function sign(beneficiary: string, api: ApiPromise, tx: SubstrateTransaction) {
+  substrateSigner.signAndSend(
+    beneficiary,
+    api,
+    tx,
+    ({ status }) => {
+      console.log(status.toHuman());
     },
     (error) => {
       console.error(error);
--- examples/sdk-esm/src/signers/evm.ts
+++ examples/sdk-esm/src/signers/evm.ts
@@ -1,15 +1,12 @@
-import { ApiPromise } from '@polkadot/api';
-
-import { EvmClient, H160 } from '@galacticcouncil/sdk';
+import { EvmClient, H160, SubstrateTransaction } from '@galacticcouncil/sdk';
 
 const DISPATCH_ADDRESS = '0x0000000000000000000000000000000000000401';
 const GAS_TO_WEIGHT = 25_000n;
 
 export async function signAndSend(
   address: string,
-  call: string,
-  api: ApiPromise,
   evm: EvmClient,
+  tx: SubstrateTransaction,
   onTransactionSend: (hash: string | null) => void,
   onTransactionReceipt: (receipt: any) => void,
   onError: (error: unknown) => void
@@ -18,70 +15,43 @@ export async function signAndSend(
   const evmClient = evm.getProvider();
   const signer = evm.getSigner(account);
 
-  console.log(account);
-
   await signer.switchChain({ id: evm.chain.id });
   await signer.request({ method: 'eth_requestAccounts' });
 
-  let data: `0x${string}` | null = null;
-  let txHash: `0x${string}` | null = null;
-  let txWeight: string | null = null;
+  const extrinsic = tx.get();
+  const { weight } = await extrinsic.paymentInfo(address);
+  const calldata = extrinsic.inner.toHex();
+  const refTime = weight.refTime.toString();
 
-  try {
-    const extrinsic = api.tx(call);
-    const info = await extrinsic.paymentInfo(address);
-    txWeight = info.weight.refTime.toString();
-    data = extrinsic.inner.toHex();
-    console.log(extrinsic.inner.toHuman());
-  } catch (error) {}
-  if (data) {
-    const [gas, gasPrice] = await Promise.all([
-      evmClient.estimateGas({
-        account: account as `0x${string}`,
-        data: data as `0x${string}`,
-        to: DISPATCH_ADDRESS as `0x${string}`,
-      }),
-      evmClient.getGasPrice(),
-    ]);
-
-    const onePrc = gasPrice / 100n;
-    const gasPricePlus = gasPrice + onePrc * 5n;
-
-    let gasLimit: bigint;
-    if (txWeight) {
-      const weight2Gas = BigInt(txWeight) / GAS_TO_WEIGHT;
-      gasLimit = (weight2Gas * 11n) / 10n;
-    } else {
-      // Fallback
-      gasLimit = (gas * 13n) / 10n;
-    }
-
-    console.log('Gas limit: ' + gasLimit);
-
-    txHash = await signer.sendTransaction({
+  const [gas, gasPrice] = await Promise.all([
+    evmClient.estimateGas({
       account: account as `0x${string}`,
-      chain: evm.chain,
-      data: data,
-      maxPriorityFeePerGas: gasPricePlus,
-      maxFeePerGas: gasPricePlus,
-      gas: gasLimit,
+      data: calldata as `0x${string}`,
       to: DISPATCH_ADDRESS as `0x${string}`,
-    });
-  } else {
-    const estGas = await evmClient.estimateGas({
-      account: address as `0x${string}`,
-      data: call as `0x${string}`,
-      to: address as `0x${string}`,
-    });
-    console.log('Est gas: ' + estGas);
+    }),
+    evmClient.getGasPrice(),
+  ]);
 
-    txHash = await signer.sendTransaction({
-      account: account as `0x${string}`,
-      chain: evm.chain,
-      data: call as `0x${string}`,
-      to: address as `0x${string}`,
-    });
+  const onePct = gasPrice / 100n;
+  const gasPriceExtra = gasPrice + onePct * 5n;
+
+  let gasLimit: bigint;
+  if (tx.extraGas) {
+    const weight2Gas = BigInt(refTime) / GAS_TO_WEIGHT;
+    gasLimit = (weight2Gas * 11n) / 10n; // add 10%
+  } else {
+    gasLimit = (gas * 13n) / 10n; // add 30%
   }
+
+  const txHash = await signer.sendTransaction({
+    account: account as `0x${string}`,
+    chain: evm.chain,
+    data: calldata,
+    maxPriorityFeePerGas: gasPriceExtra,
+    maxFeePerGas: gasPriceExtra,
+    gas: gasLimit,
+    to: DISPATCH_ADDRESS as `0x${string}`,
+  });
 
   onTransactionSend(txHash);
   evmClient
