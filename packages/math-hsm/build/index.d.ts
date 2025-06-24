abe1fe06882b0418e018f0b769bc2111f67a0a51
Pavol Noha <pavol.noha@gmail.com>
2025-06-24 10:35:55 +0200

    hsm: add math #168

---
+++ packages/math-hsm/build/index.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export function calculate_hollar_out_given_collateral_in(amount_in: string, collateral_peg: string, purchase_fee: string): string;
+export function calculate_collateral_in_given_hollar_out(amount_out: string, collateral_peg: string, purchase_fee: string): string;
+export function calculate_collateral_out_given_hollar_in(amount_in: string, execution_price: string, buyback_fee: string): string;
+export function calculate_hollar_in_given_collateral_out(amount_out: string, execution_price: string, buyback_fee: string): string;
