4ddb3f076e2f08ca8eedc950dea129c3ae6065de
cl0w <v@lery.dev>
2025-07-07 17:09:01 +0200

    fix tests

--- packages/xcm-cfg/src/chains/polkadot/neuroweb.ts
+++ packages/xcm-cfg/src/chains/polkadot/neuroweb.ts
@@ -21,7 +21,7 @@ export const neuroweb = new Parachain({
   explorer: 'https://neuroweb.subscan.io',
   genesisHash:
     '0xe7e0962324a3b86c83404dbea483f25fb5dab4c224791c81b756cfc948006174',
-  key: 'neuro',
+  key: 'neuroweb',
   name: 'Neuroweb',
   parachainId: 2043,
   ss58Format: 101,
