76ca2949fa28072aebe3ae379c510c845c7825a7
Pavol Noha <pavol.noha@gmail.com>
2025-08-11 16:07:38 +0200

    sui: transfer configs

--- packages/xcm-cfg/src/assets.ts
+++ packages/xcm-cfg/src/assets.ts
@@ -185,6 +185,11 @@ export const sol = new Asset({
   originSymbol: 'SOL',
 });
 
+export const sui = new Asset({
+  key: 'sui',
+  originSymbol: 'SUI',
+});
+
 export const sub = new Asset({
   key: 'sub',
   originSymbol: 'SUB',
@@ -358,6 +363,7 @@ export const assets: Asset[] = [
   ring,
   sky,
   sol,
+  sui,
   sub,
   susde,
   susds,
