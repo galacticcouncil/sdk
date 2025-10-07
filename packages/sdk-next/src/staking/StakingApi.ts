e2c1241a63bfafdef6e2c1a4784706757c59c7e8
Radoslav Karlík <radoslav.karlik@cleevio.com>
2025-10-07 09:24:55 +0200

    Extend staking API

--- packages/sdk-next/src/staking/StakingApi.ts
+++ packages/sdk-next/src/staking/StakingApi.ts
@@ -3,12 +3,41 @@ import { encodeAddress } from '@polkadot/util-crypto';
 import { stringToU8a } from '@polkadot/util';
 import {
   calculate_accumulated_rps,
+  calculate_percentage_amount,
   calculate_period_number,
+  calculate_points,
   calculate_rewards,
+  sigmoid,
 } from '@galacticcouncil/math-staking';
 import Big from 'big.js';
 import { StakingClient } from './StakingClient';
 import { BalanceClient } from 'client';
+import { Balance } from 'types';
+
+const CONVICTIONS = {
+  none: 0.1,
+  locked1x: 1,
+  locked2x: 2,
+  locked3x: 3,
+  locked4x: 4,
+  locked5x: 5,
+  locked6x: 6,
+} as const satisfies { [key: string]: number };
+
+type Conviction = keyof typeof CONVICTIONS;
+
+const isConviction = (conviction: string): conviction is Conviction =>
+  Object.keys(CONVICTIONS).includes(conviction);
+
+type TVote = {
+  id: number;
+  amount: bigint;
+  conviction: Conviction;
+};
+
+/* constants that might be changed */
+const a = '20000000000000000';
+const b = '2000';
 
 export const BIG_10 = Big(10);
 export const BIG_BILL = Big(BIG_10.pow(12));
@@ -29,14 +58,14 @@ export class StakingApi {
     this.balanceClient = balanceClient;
   }
 
-  async getFreePotBalance(): Promise<bigint> {
+  async getPotBalance(): Promise<Balance> {
     const palletId = await this.client.getPalletId();
     const potAddress = getHydraAccountAddress(palletId);
     const balance = await this.balanceClient.getBalance(
       potAddress,
       SYSTEM_ASSET_ID
     );
-    return balance.transferable;
+    return balance;
   }
 
   async getStakingPosition(id: bigint) {
@@ -45,44 +74,48 @@ export class StakingApi {
       this.client.getStakingVotes(id),
     ]);
 
-    const createdAt = positionData?.created_at;
+    if (!positionData) {
+      return undefined;
+    }
 
-    const votes: Array<{
-      id: number;
-      amount: bigint;
-      conviction: string;
-    }> = await votesRes.reduce(async (acc, [key, data]) => {
-      const prevAcc = await acc;
-      const id = key;
-      const amount = data.amount;
-      const conviction = data.conviction.toString();
+    const createdAt = positionData.created_at;
 
-      const referendaInfo = await this.client.getReferendumInfo(id);
+    const votes: Array<TVote> = await votesRes.reduce(
+      async (acc, [key, data]) => {
+        const prevAcc = await acc;
+        const id = key;
+        const amount = data.amount;
+        const conviction = data.conviction.toString().toLowerCase();
 
-      if (!referendaInfo) return prevAcc;
+        const referendaInfo = await this.client.getReferendumInfo(id);
 
-      if (
-        referendaInfo.type === 'Approved' ||
-        referendaInfo.type === 'Rejected'
-      ) {
-        prevAcc.push({
-          id,
-          amount,
-          conviction,
-        });
-      }
+        if (!referendaInfo) return prevAcc;
 
-      return prevAcc;
-    }, Promise.resolve<Array<{ id: number; amount: bigint; conviction: string }>>([]));
+        if (
+          (referendaInfo.type === 'Approved' ||
+            referendaInfo.type === 'Rejected') &&
+          isConviction(conviction)
+        ) {
+          prevAcc.push({
+            id,
+            amount,
+            conviction,
+          });
+        }
+
+        return prevAcc;
+      },
+      Promise.resolve<Array<TVote>>([])
+    );
 
     return {
-      stake: positionData?.stake,
-      rewardPerStake: positionData?.reward_per_stake,
+      stake: positionData.stake,
+      rewardPerStake: positionData.reward_per_stake,
       createdAt,
-      actionPoints: positionData?.action_points,
-      accumulatedUnpaidRewards: positionData?.accumulated_unpaid_rewards,
-      accumulatedSlashPoints: positionData?.accumulated_slash_points,
-      accumulatedLockedRewards: positionData?.accumulated_locked_rewards,
+      actionPoints: positionData.action_points,
+      accumulatedUnpaidRewards: positionData.accumulated_unpaid_rewards,
+      accumulatedSlashPoints: positionData.accumulated_slash_points,
+      accumulatedLockedRewards: positionData.accumulated_locked_rewards,
       votes: votes,
     };
   }
@@ -108,10 +141,88 @@ export class StakingApi {
     };
   }
 
+  getCurrentActionPoints(
+    votes: TVote[],
+    initialActionPoints: bigint,
+    stakePosition: bigint,
+    activeReferendaIds: string[]
+  ) {
+    let currentActionPoints = Big(0);
+    let maxActionPoints = Big(0);
+
+    const maxConviction = CONVICTIONS['locked6x'];
+    const maxVotingPower = Big(stakePosition.toString()).mul(maxConviction);
+    const maxActionPointsPerRef = 100;
+
+    const votedActiveReferendaIds: string[] = [];
+
+    votes.forEach((vote) => {
+      const convictionIndex = CONVICTIONS[vote.conviction];
+
+      const isActiveReferenda = activeReferendaIds.includes(vote.id.toString());
+
+      if (isActiveReferenda) {
+        votedActiveReferendaIds.push(vote.id.toString());
+      }
+
+      const actionPointsBase = Big(vote.amount.toString())
+        .mul(maxActionPointsPerRef)
+        .div(maxVotingPower);
+
+      currentActionPoints = currentActionPoints.plus(
+        Math.floor(actionPointsBase.mul(convictionIndex).toNumber())
+      );
+
+      maxActionPoints = maxActionPoints.plus(
+        Math.floor(
+          actionPointsBase
+            .mul(isActiveReferenda ? maxConviction : convictionIndex)
+            .toNumber()
+        )
+      );
+    });
+
+    const stakePositionAddition = Math.floor(
+      Big(stakePosition.toString())
+        .mul(maxConviction)
+        .mul(maxActionPointsPerRef)
+        .div(maxVotingPower)
+        .toNumber()
+    );
+
+    activeReferendaIds.forEach((activeReferendaId) => {
+      if (!votedActiveReferendaIds.includes(activeReferendaId)) {
+        maxActionPoints = maxActionPoints.plus(stakePositionAddition);
+      }
+    });
+
+    const actionMultipliers = {
+      democracyVote: 1,
+    };
+
+    currentActionPoints = currentActionPoints.mul(
+      actionMultipliers.democracyVote
+    );
+    currentActionPoints = currentActionPoints.plus(
+      initialActionPoints.toString() || '0'
+    );
+
+    maxActionPoints = maxActionPoints.mul(actionMultipliers.democracyVote);
+    maxActionPoints = maxActionPoints.plus(
+      initialActionPoints.toString() || '0'
+    );
+
+    return {
+      currentActionPoints: currentActionPoints.toString(),
+      maxActionPoints: maxActionPoints.toString(),
+    };
+  }
+
   async getRewards(
     account: string,
+    activeReferendaIds: Array<string>,
     blockNumber: string
-  ): Promise<string | undefined> {
+  ) {
     const stake = await this.getStake(account);
 
     const {
@@ -125,57 +236,104 @@ export class StakingApi {
       return undefined;
     }
 
-    const [freePotbalance, periodLength, unclaimablePeriods, sixBlockSince] =
-      await Promise.all([
-        this.getFreePotBalance(),
-        this.client.getPeriodLength(),
-        this.client.getUnclaimablePeriods(),
-        this.client.getSixBlockSince(),
-      ]);
+    const [
+      potBalance,
+      periodLength,
+      unclaimablePeriods,
+      timePointsPerPeriod,
+      timePointsWeight,
+      actionPointsWeight,
+      sixBlockSince,
+    ] = await Promise.all([
+      this.getPotBalance(),
+      this.client.getPeriodLength(),
+      this.client.getUnclaimablePeriods(),
+      this.client.getTimePointsPerPeriod(),
+      this.client.getTimePointsWeight(),
+      this.client.getActionPointsWeight(),
+      this.client.getSixBlockSince(),
+    ]);
 
-    const pendingRewards = Big(freePotbalance.toString()).minus(
+    const pendingRewards = Big(potBalance.transferable.toString()).minus(
       potReservedBalance.toString()
     );
 
-    let rewardPerStake = accumulatedRewardPerStake.toString();
-    const sixBlockFallback = Big(blockNumber).plus(1).toString();
-
-    if (pendingRewards.gt(0) && totalStake > 0) {
-      rewardPerStake = calculate_accumulated_rps(
-        accumulatedRewardPerStake.toString(),
-        pendingRewards.toString(),
-        totalStake.toString()
-      );
-    }
+    const rewardPerStake =
+      pendingRewards.gt(0) && totalStake > 0
+        ? calculate_accumulated_rps(
+            accumulatedRewardPerStake.toString(),
+            pendingRewards.toString(),
+            totalStake.toString()
+          )
+        : accumulatedRewardPerStake.toString();
 
     const currentPeriod = calculate_period_number(
       periodLength.toString(),
       blockNumber,
-      sixBlockSince ?? sixBlockFallback
+      sixBlockSince
     );
 
     const enteredAt = calculate_period_number(
       periodLength.toString(),
-      stakePosition.createdAt?.toString() ?? '',
-      sixBlockSince ?? sixBlockFallback
+      stakePosition.createdAt.toString(),
+      sixBlockSince
     );
 
     const maxRewards = calculate_rewards(
       rewardPerStake,
-      stakePosition.rewardPerStake?.toString() ?? '',
-      stakePosition.stake?.toString() ?? ''
+      stakePosition.rewardPerStake.toString(),
+      stakePosition.stake.toString()
     );
 
+    const actionPoints = this.getCurrentActionPoints(
+      stakePosition.votes,
+      stakePosition.actionPoints,
+      stakePosition.stake,
+      activeReferendaIds
+    );
+
+    const points = calculate_points(
+      enteredAt,
+      currentPeriod,
+      timePointsPerPeriod.toString(),
+      timePointsWeight.toString(),
+      actionPoints.currentActionPoints,
+      actionPointsWeight.toString(),
+      stakePosition.accumulatedSlashPoints.toString()
+    );
+
+    const payablePercentage = sigmoid(points, a, b);
+
     const totalRewards = Big(maxRewards)
-      .plus(stakePosition.accumulatedUnpaidRewards?.toString() || '0')
-      .plus(stakePosition.accumulatedLockedRewards?.toString() || '0');
+      .plus(stakePosition.accumulatedUnpaidRewards.toString())
+      .plus(stakePosition.accumulatedLockedRewards.toString());
 
     if (
       Big(currentPeriod).minus(enteredAt).lte(unclaimablePeriods.toString())
     ) {
-      return undefined;
+      return { rewards: '0' };
     }
 
-    return totalRewards.div(BIG_BILL).toString();
+    const userRewards = calculate_percentage_amount(
+      totalRewards.toString(),
+      payablePercentage
+    );
+
+    const accumulatedLockedRewardsBig = Big(
+      stakePosition.accumulatedLockedRewards.toString()
+    );
+
+    const availabeRewards = accumulatedLockedRewardsBig.gt(userRewards)
+      ? accumulatedLockedRewardsBig
+      : Big(userRewards);
+
+    return {
+      rewards: availabeRewards.div(BIG_BILL).toString(),
+      maxRewards: totalRewards.div(BIG_BILL).toString(),
+      allocatedRewardsPercentage: availabeRewards
+        .div(totalRewards)
+        .mul(100)
+        .toNumber(),
+    };
   }
 }
