702a1044d27ff7c957636a20b25a348806eea064
Pavol Noha <pavol.noha@gmail.com>
2025-11-10 22:55:56 -0300

    fix: papi (again) :(

--- packages/sdk-next/test/script/types.ts
+++ packages/sdk-next/test/script/types.ts
@@ -2,5 +2,6 @@ export enum ApiUrl {
   Local = 'ws://localhost:8000',
   Chopsticks = 'wss://chopsticks.rpc.hydration.cloud',
   Hydration = 'wss://hydration-rpc.n.dwellir.com',
+  Latam = 'wss://hydration.rpc.stkd.io',
   Nice = 'wss://rpc.nice.hydration.cloud',
 }
