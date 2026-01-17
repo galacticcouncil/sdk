76c36922124464d0f15082ab8dd71cc9b5ecd967
Pavol Noha <pavol.noha@gmail.com>
2026-01-17 12:49:48 +0100

    common: metadata utils (error decoder)

--- packages/common/src/utils/index.ts
+++ packages/common/src/utils/index.ts
@@ -4,6 +4,7 @@ export * as erc20 from './erc20';
 export * as h160 from './h160';
 export * as hex from './hex';
 export * as log from './log';
+export * as meta from './meta';
 export * as xcm from './xcm';
 export * as xc from './xc';
 
