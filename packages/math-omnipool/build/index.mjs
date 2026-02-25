2577b18b9ddcac584c33fd3f64207a35b6406db0
Pavol Noha <pavol.noha@gmail.com>
2026-02-25 11:25:59 +0000

    slip based fee support

--- packages/math-omnipool/build/index.mjs
+++ packages/math-omnipool/build/index.mjs
@@ -2,3 +2,4 @@ import * as wasm from "./hydra_dx_wasm_bg.wasm";
 export * from "./hydra_dx_wasm_bg.js";
 import { __wbg_set_wasm } from "./hydra_dx_wasm_bg.js";
 __wbg_set_wasm(wasm);
+wasm.__wbindgen_start();
