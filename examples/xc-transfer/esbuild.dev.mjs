dbac6c64f8a9efef223571e143ccf0313c84c06e
Pavol Noha <pavol.noha@gmail.com>
2026-05-19 11:27:15 +0200

    xc: nintent poc

--- examples/xc-transfer/esbuild.dev.mjs
+++ examples/xc-transfer/esbuild.dev.mjs
@@ -7,6 +7,7 @@ const plugins = [wasmLoader({ mode: 'deferred' })];
 const options = {
   entryPoints: [
     'src/index.ts',
+    'src/intent.ts',
     'src/redeem.ts',
     'src/scan.ts',
     'src/circuitbreaker.ts',
