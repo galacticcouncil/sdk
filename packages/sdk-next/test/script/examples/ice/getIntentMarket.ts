101f93dbf17a8989735791b6fc38dfe01d4b7e88
Pavol Noha <pavol.noha@gmail.com>
2026-05-20 13:29:27 +0200

    ice: test scripts

---
+++ packages/sdk-next/test/script/examples/ice/getIntentMarket.ts
@@ -0,0 +1,44 @@
+import { Binary, PolkadotClient } from 'polkadot-api';
+
+import { createSdkContext } from '../../../../src';
+
+import { PapiExecutor } from '../../PapiExecutor';
+import { BENEFICIARY } from '../../const';
+import { ApiUrl } from '../../types';
+
+class GetIntentMarket extends PapiExecutor {
+  async script(client: PolkadotClient) {
+    const sdk = await createSdkContext(client);
+
+    const { api, tx } = sdk;
+
+    const trade = await api.router.getBestSell(10, 0, 1000_000_000n);
+    const intentTx = await tx
+      .intentMarket(trade)
+      .withBeneficiary(BENEFICIARY)
+      .withSlippage(0.5)
+      .build();
+
+    const intentCall = await intentTx.get().getEncodedData();
+    console.log(trade.toHuman());
+    console.log('Tx name: ' + intentTx.name);
+    console.log('Transaction hash: ' + Binary.toHex(intentCall));
+
+    try {
+      const dryRun = await intentTx.dryRun(BENEFICIARY);
+      if (dryRun.success) {
+        const { execution_result } = dryRun.value;
+        console.log('Transaction status: ' + execution_result.success);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+
+    return () => {
+      sdk.destroy();
+      client.destroy();
+    };
+  }
+}
+
+new GetIntentMarket(ApiUrl.Lark3, 'Get Intent market swap').run();
---
+++ packages/sdk-next/test/script/examples/ice/getIntentOrderTwapBuy.ts
@@ -0,0 +1,44 @@
+import { Binary, PolkadotClient } from 'polkadot-api';
+
+import { createSdkContext } from '../../../../src';
+
+import { PapiExecutor } from '../../PapiExecutor';
+import { BENEFICIARY } from '../../const';
+import { ApiUrl } from '../../types';
+
+class GetIntentOrderTwapBuy extends PapiExecutor {
+  async script(client: PolkadotClient) {
+    const sdk = await createSdkContext(client);
+
+    const { api, tx } = sdk;
+
+    const order = await api.scheduler.getTwapBuyOrder(10, 0, '1000');
+    const intentTx = await tx
+      .intentOrder(order)
+      .withBeneficiary(BENEFICIARY)
+      .withSlippage(1)
+      .build();
+
+    const intentCall = await intentTx.get().getEncodedData();
+    console.log(order.toHuman());
+    console.log('Tx name: ' + intentTx.name);
+    console.log('Transaction hash: ' + Binary.toHex(intentCall));
+
+    try {
+      const dryRun = await intentTx.dryRun(BENEFICIARY);
+      if (dryRun.success) {
+        const { execution_result } = dryRun.value;
+        console.log('Transaction status: ' + execution_result.success);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+
+    return () => {
+      sdk.destroy();
+      client.destroy();
+    };
+  }
+}
+
+new GetIntentOrderTwapBuy(ApiUrl.Lark3, 'Get Intent TWAP BUY order').run();
